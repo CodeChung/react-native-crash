e9d5c1cec8dde1a9bf7383a148ddf68c382156fe
Harry Chung <hc9825@gmail.com>
2019-11-13 11:49:09 -0800

    list set up

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -1,18 +1,59 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View, TextInput, } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, View, TextInput, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!')
-  return (
-    <View style={{ padding: 50 }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-        <TextInput placeholder='Course Goal' style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10 }}/>
-        <Button title='ADD' />
-      </View>
-      <View>
+  const [enteredGoal, setEnteredGoal] = useState('')
+  const [courseGoals, setCourseGoals] = useState([])
 
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText)
+  }
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [
+      ...currentGoals, 
+      { key: Math.random().toString(), value: enteredGoal }
+    ])
+  }
+
+  return (
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput 
+          placeholder='Course Goal' 
+          style={styles.input} 
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title='ADD' onPress={addGoalHandler} />
       </View>
+      <FlatList 
+        data={courseGoals} 
+        renderItem={itemData => (
+          <GoalItem>
+            {itemData.item.value}
+          </GoalItem>
+        )} 
+      />
     </View>
   );
 }
- 
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%', 
+    borderColor: 'black', 
+    borderWidth: 1, 
+    padding: 10
+  },
+
+})
